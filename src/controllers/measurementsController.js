ddea9eb21e59d0f782f073443fb254df4e0e95c3
tferrandis <ferrandis.cayetano@gmail.com>
2025-05-07 18:57:13 +0200

    change for the token

--- src/controllers/measurementsController.js
+++ src/controllers/measurementsController.js
@@ -1,16 +1,17 @@
 const Measurement = require('../models/measurement');
-	
+
+// Crear una nueva medición usando user_id del token
 exports.createMeasurement = async (req, res) => {
-  const { _id, device_id, user_id, timestamp, location, measurements } = req.body;
+  const { _id, device_id, timestamp, location, measurements } = req.body;
+
   try {
     const measurement = new Measurement({
       _id,
-      device_id,
-      user_id,
       timestamp,
       location,
       measurements,
     });
+
     await measurement.save();
     res.status(201).json(measurement);
   } catch (error) {
@@ -18,17 +19,18 @@ exports.createMeasurement = async (req, res) => {
   }
 };
 
+// Obtener todas las mediciones del usuario autenticado
 exports.getMeasurementsByUserId = async (req, res) => {
-    const { user_id } = req.params; // Obtener el user_id de los parámetros de la URL
-    try {
-      const measurements = await Measurement.find({ user_id });
-      if (!measurements.length) {
-        return res.status(404).json({ message: 'No se encontraron mediciones para este usuario' });
-      }
-      res.status(200).json(measurements);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  };
+  try {
+    const userId = req.user._id; 
+    const measurements = await Measurement.find({ user_id: userId });
 
-// Aquí podrías agregar otros métodos para gestionar las mediciones, como obtener una medición por ID, etc.
+    if (!measurements.length) {
+      return res.status(404).json({ message: 'No se encontraron mediciones para este usuario' });
+    }
+
+    res.status(200).json(measurements);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
