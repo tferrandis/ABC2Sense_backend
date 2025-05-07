df1c765a5f538e7b4cf49867311e1f6a45d0ed11
tferrandis <ferrandis.cayetano@gmail.com>
2025-05-07 18:48:33 +0200

    cambio validador

--- src/validators/measurementsValidators.js
+++ src/validators/measurementsValidators.js
@@ -1,8 +1,6 @@
 const { check, body, validationResult } = require('express-validator');
 
 const measurementValidator = [
-  check('device_id').isMongoId().withMessage('El device_id debe ser un ObjectId válido'),
-  check('user_id').isMongoId().withMessage('El user_id debe ser un ObjectId válido'),
   check('timestamp').isISO8601().toDate().withMessage('El timestamp debe ser una fecha válida'),
   body('location.lat').optional().isNumeric().withMessage('La latitud debe ser un número'),
   body('location.long').optional().isNumeric().withMessage('La longitud debe ser un número'),
