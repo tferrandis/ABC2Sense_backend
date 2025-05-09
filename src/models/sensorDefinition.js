9cebb5372ebeadac22e6821f42f82aee98a64208
tferrandis <ferrandis.cayetano@gmail.com>
2025-05-09 18:07:18 +0200

    add unity to sensor

--- src/models/sensorDefinition.js
+++ src/models/sensorDefinition.js
@@ -4,7 +4,8 @@ const SensorDefinitionSchema = new Schema({
   sensorId: { type: Number, required: true, unique: true }, 
   title: { type: String, required: true },
   description: { type: String }, 
-  measure: { type: Number, required: true } 
+  measure: { type: Number, required: true },
+  unit : {type: String,  required: true}
 });
 
 const SensorDefinition = model("SensorDefinition", SensorDefinitionSchema);
