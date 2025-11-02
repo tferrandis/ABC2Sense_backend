8dee006df85874efe5d90af325b8303812eb6557
tferrandis <ferrandis.cayetano@gmail.com>
2025-11-02 10:55:14 +0100

    add server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-<<<<<<< HEAD
 const passport = require('./config/passportService');  
 const dotenv = require('dotenv');
 const fs = require('fs');
@@ -8,34 +7,12 @@ const path = require('path');
 
 dotenv.config();
 
-=======
-const passport = require('passport');
-const dotenv = require('dotenv');
-const authRoutes = require('./routes/authRoutes');
-const sensorRoutes = require('./routes/sensorRoutes');
-
-
-
-require('./config/passportService');
-
-
-
-dotenv.config();
-
-console.log("Mongo URI:", process.env.MONGO_URI); // Agrega esto para depurar
-
-
-
->>>>>>> 1f19f5b965fef7b855a945d670909bc315239476
 const app = express();
 
 app.use(express.json());
 
-<<<<<<< HEAD
 app.use(passport.initialize());
 
-=======
->>>>>>> 1f19f5b965fef7b855a945d670909bc315239476
 console.log("Connecting to mongodb...");
 
 (async () => {
@@ -44,7 +21,6 @@ console.log("Connecting to mongodb...");
         console.log('MongoDB connected')
         app.use(passport.initialize());
 
-<<<<<<< HEAD
         const routesPath = path.join(__dirname, 'routes');
         fs.readdirSync(routesPath).forEach((file) => {
           if (file.endsWith('.js')) {
@@ -63,15 +39,6 @@ console.log("Connecting to mongodb...");
 
         const PORT = process.env.PORT || 5000;
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-=======
-        // Use routes
-        app.use('/api/auth', authRoutes);
-        app.use('/api', sensorRoutes);
-
-
-        const PORT = process.env.PORT || 5000;
-        app.listen(PORT,'0.0.0.0', () => console.log(`Server running on port ${PORT}`));
->>>>>>> 1f19f5b965fef7b855a945d670909bc315239476
     } catch (e) {
         console.log(e);
     }
