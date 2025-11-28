19f3ba5c1d23b57c19256607159ec1366eb97900
tferrandis <ferrandis.cayetano@gmail.com>
2025-11-28 18:40:30 +0100

    admin middleware

---
+++ src/middlewares/isAdmin.js
@@ -0,0 +1,7 @@
+// middlewares/isAdmin.js
+module.exports = (req, res, next) => {
+  if (req.user && req.user.role === 'admin') {
+    return next();
+  }
+  return res.status(403).json({ message: 'Acceso denegado: solo administradores' });
+};
