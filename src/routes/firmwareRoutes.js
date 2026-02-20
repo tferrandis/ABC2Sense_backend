173e89345b7e6f39fb58bc0b590cd45347138109
racar <racar@local.dev>
2026-02-20 02:47:47 +0100

    feat(firmware): add OTA catalog endpoint and OTA event logging

--- src/routes/firmwareRoutes.js
+++ src/routes/firmwareRoutes.js
@@ -4,11 +4,13 @@ const firmwareController = require('../controllers/firmwareController');
 const adminAuth = require('../middleware/adminAuth');
 
 // Public routes (for IoT devices)
+router.get('/catalog', firmwareController.getCatalog);
 router.get('/latest', firmwareController.getLatest);
 router.get('/download/:id', firmwareController.download);
 
 // Protected routes (require admin authentication)
 router.post('/upload', adminAuth, firmwareController.upload);
+router.get('/events', adminAuth, firmwareController.listEvents);
 router.get('/', adminAuth, firmwareController.list);
 router.get('/:id', adminAuth, firmwareController.getById);
 router.put('/:id/activate', adminAuth, firmwareController.setActive);
